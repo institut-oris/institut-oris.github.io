d0982d853ec229934abaf77895952a02b4ecda18
Pika <pika.dotty@gmail.com>
2024-05-09 11:15:14 +0200

    Remove partners.

--- src/components/partners/partners.tsx
+++ src/components/partners/partners.tsx
@@ -14,13 +14,7 @@ export const Partners = ({ title }: PartnersProps): JSX.Element => {
         <Image src='/partners/rokus.png' alt='Rokus' width={172} height={57} />
         <Image src='/partners/rutina.png' alt='Rutina' width={152} height={57} />
         <Image src='/partners/ragor.png' alt='Ragor' width={210} height={110} />
-
-        <Image src='/partners/alpine-nation.png' alt='alpine-nation' width={160} height={54} />
-        <Image src='/partners/tipovej.png' alt='Tipovej!org' width={110} height={30} />
         <Image src='/partners/plezarna.png' alt='Plezarna' width={197} height={197} />
-        <Image src='/partners/risa.png' alt='Risa' width={146} height={97} />
-        <Image src='/partners/cpm.png' alt='Center za pomoč mladim' width={67} height={126} />
-        <Image src='/partners/octopics.png' alt='Octopics' width={160} height={120} />
       </div>
     </PageSection>
   );
